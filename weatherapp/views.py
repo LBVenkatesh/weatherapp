660d8756b051586700c2ae567e5c5519f6430688
LBVenkatesh <venkateshlb11699@gmail.com>
2019-04-06 23:22:30 +0530

    First commit

---
+++ weatherapp/views.py
@@ -0,0 +1,27 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+# Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=ab140c6460c04e85b151704982d300c9'
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weatherapp/weather.html', context)
